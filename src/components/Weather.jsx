9e4daa61c184bcd880b1ca51c5880e6d3edda7af
Adrian Rak <adrrak@tlen.pl>
2018-07-31 23:08:22 +0200

    display of all temperatures

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -9,28 +9,39 @@ export default class WeatherApp extends React.Component {
         this.state = {
             forecast: [],
             mainWeather: [],
-            tempMax: []
+            tempMax: [],
+            weather: []
         }
     }
 
     componentDidMount() {
+     this.getData();
+    }
+
+    getData=()=>{
         fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Warsaw&APPID=75883e1e36c82d2615632b909385e893`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.list[0]);
-                let forecast = data;
-                console.log(forecast);
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.list[0]);
+            let forecast = data;
+            console.log('forecast', forecast);
 
-                let mainWeather = data.list[0].main;
-                console.log(mainWeather);
-                let tempMax = Math.round((data.list[0].main.temp_max)-273.15);
-                console.log(data.list[0].main.temp);
-                console.log(tempMax);
+            let mainWeather = data.list[0].main;
+            console.log(mainWeather);
+            let tempMax = Math.round((data.list[0].main.temp_max)-273.15);
+            console.log(data.list[0].main.temp);
+            console.log(tempMax);
+            console.log('data list', data.list);
+            let weather = data.list;
+            console.log('weatherOne', weather[1]);
 
-                this.setState({forecast: forecast});
-                this.setState({tempMax: tempMax});
-            })
-            .catch(error => console.log('error', error))
+
+
+            this.setState({forecast: forecast});
+            this.setState({tempMax: tempMax});
+            this.setState({weather: weather});
+        })
+        .catch(error => console.log('error', error))
     }
   
     onChangeHandle(event) {
@@ -62,7 +73,12 @@ export default class WeatherApp extends React.Component {
                     <OneDayLook />
                 </div>
                 <div>
-                    Pogoda jest taka jak widać: {this.state.tempMax}&deg; C;
+                    {/* Pogoda jest taka jak widać: {this.state.tempMax}&deg; C */}
+                    Pogoda jest taka jak widać: {this.state.weather.map((data) => {
+                        return (
+                            <li>{Math.round(data.main.temp_max-273.15)}&deg; C</li>
+                        )
+                    })}
                 </div>
             </div>
         )
